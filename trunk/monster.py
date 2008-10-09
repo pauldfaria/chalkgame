319645ce72a24687e743717b387a987d6d42a5cb
retsamff <retsamff@9cb68c4e-851d-11dd-8a74-1916a93a1be7>
2008-10-09 01:50:21 +0000

    box will die if it goes off the screen. box will follow character around. This could be better, it depends if the enemies will all follow this routine or some will stay back and use ranged attacks, etc. Added a kills counter because I wanted to know how many of those boxes I killed :D

--- trunk/monster.py
+++ trunk/monster.py
@@ -38,4 +38,26 @@ class Monster(Player):
     def refresh(self):
         # I realized here, that all monsters need to know
         # where the players are, but we don't have that =p
-        pass
+        # pass
+
+        # I added back some of the same code from the player class because
+        # we don't want the monsters to be going off the screen - Calvin
+        # also, now the box dies when it goes off the left and can be respawned
+        
+        self.pos.right += self.speed[0]
+        self.pos.left += self.speed[0]
+        self.pos.top += self.speed[1]
+        self.pos.bottom += self.speed[1]
+        if (self.pos.right > self.screen[0]):
+            #self.pos.right = self.screen[0]
+            self.speed[0] = 0
+        elif (self.pos.left < 0):
+            self.kill()
+            return False
+        elif (self.pos.top < (self.screen[1] * 5 / 8)):
+            self.pos.top = (self.screen[1] * 5 / 8)
+            self.speed[1] = 0
+        elif (self.pos.bottom > self.screen[1]):
+            self.pos.bottom = self.screen[1]
+            self.speed[1] = 0
+        return True
